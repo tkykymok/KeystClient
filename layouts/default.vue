a3282245e5f6c11f9c94736b930aebb9605face3
takayukiyamaoka <tkykymok@me.com>
2021-05-22 12:04:42 +0900

    初回コミット

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <sidebar />
+    <div class="relative md:ml-64 bg-blueGray-100">
+      <div class="px-4 md:px-10 mx-auto w-full -m-24">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+import Sidebar from "@/components/Sidebar/Sidebar.vue";
+
+@Component({
+  name: "default-layout",
+  components: {
+    Sidebar,
+  },
+})
+export default class extends Vue {
+};
+</script>
