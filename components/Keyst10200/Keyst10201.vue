1b6b7c1e5133e63dca304afbf8f4e6c2be2b09d7
takayukiyamaoka <tkykymok@me.com>
2021-05-23 14:35:39 +0900

    スキルシートヘッダー部を作成した

---
+++ components/Keyst10200/Keyst10201.vue
@@ -0,0 +1,65 @@
+<template>
+  <table class='w-3/4 table-fixed'>
+    <tr>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12'>ふりがな</th>
+      <td class='p-3 text-gray-800 border border-b' colspan='2'>やまおか たかゆき</td>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-1/12'>性別</th>
+      <td class='p-3 text-gray-800 border border-b'>男</td>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-1/12'>年齢</th>
+      <td class='p-3 text-gray-800 border border-b'>30</td>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12'>生年月日</th>
+      <td class='p-3 text-gray-800 border border-b' colspan='2'>1990年6月13日</td>
+    </tr>
+    <tr>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12'>氏名</th>
+      <td class='p-3 text-gray-800 border border-b' colspan='2'>山岡 嵩裕輝</td>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-1/12'>国籍</th>
+      <td class='p-3 text-gray-800 border border-b'>test</td>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-1/12'>配偶者</th>
+      <td class='p-3 text-gray-800 border border-b'>text</td>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12'>最寄駅</th>
+      <td class='p-3 text-gray-800 border border-b' colspan='2'>○○○　駅</td>
+    </tr>
+    <tr>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12'>最終学歴</th>
+      <td class='p-3 text-gray-800 border border-b' colspan='2'>2020年3月</td>
+      <td class='p-3 text-gray-800 border border-b' colspan='7'>○○大学　卒業</td>
+    </tr>
+    <tr>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12'>資格</th>
+      <td class='p-3 text-gray-800 border border-b' colspan='2'>2020年3月</td>
+      <td class='p-3 text-gray-800 border border-b' colspan='7'>○○○○　取得</td>
+    </tr>
+    <tr>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12'>得意分野</th>
+      <td class='p-3 text-gray-800 border border-b' colspan='9'>
+        <textarea class='w-full p-2 border-2 border-gray-300 active:outline-none focus:outline-none focus:shadow-outline'></textarea>
+      </td>
+    </tr>
+    <tr>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12'>PR</th>
+      <td class='p-3 text-gray-800 border border-b' colspan='9'>
+        <textarea class='w-full p-2 border-2 border-gray-300 active:outline-none focus:outline-none focus:shadow-outline'></textarea>
+      </td>
+    </tr>
+    <tr>
+      <th class='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12'>営業コメント</th>
+      <td class='p-3 text-gray-800 border border-b' colspan='9'>
+        <textarea class='w-full p-2 border-2 border-gray-300 active:outline-none focus:outline-none focus:shadow-outline'></textarea>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component({
+  name: 'Keyst10201'
+})
+export default class extends Vue {
+
+}
+</script>
+
+<style></style>
---
+++ pages/Keyst10200/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='p-4'>
+    <Keyst10201 />
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'nuxt-property-decorator';
+import Keyst10201 from '~/components/Keyst10200/Keyst10201';
+
+@Component({
+  name: 'Keyst10200',
+  components: {
+    Keyst10201
+  }
+})
+export default class extends Vue {
+
+}
+</script>
+
+<style></style>
