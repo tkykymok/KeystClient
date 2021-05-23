14aa5bbf5e52dc0b7ad30a8a98b64181ee7be7f4
takayukiyamaoka <tkykymok@me.com>
2021-05-23 13:05:03 +0900

    Header部品を作成した

---
+++ components/Header/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- Navbar -->
+  <nav
+    class="top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4 bg-green-500"
+  >
+    <div
+      class="w-full mx-auto items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4"
+    >
+      <!-- Brand -->
+      <a
+        class="text-gray-800 text-sm uppercase hidden lg:inline-block font-semibold"
+        href="javascript:void(0)"
+      >
+        Dashboard
+      </a>
+
+      <!-- User -->
+      <ul class="flex-col md:flex-row list-none items-center hidden md:flex">
+        <UserDropdown />
+      </ul>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'nuxt-property-decorator';
+import UserDropdown from '~/components/Dropdowns/UserDropdown.vue';
+
+@Component({
+  components: {
+    UserDropdown
+  }
+})
+export default class Header extends Vue {
+
+
+
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,24 @@
 <template>
   <div>
-    <sidebar />
-    <div class="relative md:ml-64 bg-blueGray-100">
-      <div class="px-4 md:px-10 mx-auto w-full -m-24">
-        <router-view />
+    <Sidebar />
+    <div class='md:ml-64 bg-gray-100'>
+      <Navbar />
+      <div class='px-4 md:px-10 mx-auto w-full -m-24'>
+        <Nuxt />
       </div>
     </div>
   </div>
 </template>
-<script lang="ts">
+<script lang='ts'>
 import { Component, Vue } from 'nuxt-property-decorator';
-import Sidebar from "@/components/Sidebar/Sidebar.vue";
+import Sidebar from '@/components/Sidebar/Sidebar.vue';
+import Navbar from '~/components/Header/Header.vue';
 
 @Component({
-  name: "default-layout",
+  name: 'default-layout',
   components: {
-    Sidebar,
-  },
+    Sidebar, Navbar
+  }
 })
 export default class extends Vue {
 };
