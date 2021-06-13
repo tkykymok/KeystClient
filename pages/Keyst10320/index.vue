bd982c1b2d0bd5b22d8c5d4f531270a3c0dbeaf7
riku tanaka <rikus21014@gmail.com>
2021-06-13 10:35:41 +0900

    レイアウト作成

---
+++ pages/Keyst10320/index.vue
@@ -0,0 +1,30 @@
+// ユーザー側
+<template>
+  <div class='p-3'>
+    <div>
+    <p class='p-1  w-20 text-center border-2 border-gray-300 bg-white rounded-md'>
+      Aチーム
+    </p>
+    </div>
+    <keyst10303 />
+
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'nuxt-property-decorator';
+import keyst10303 from '~/components/Keyst10300/Keyst10303.vue';
+
+@Component({
+  name: 'Keyst10320',
+  components: {
+    keyst10303
+  }
+})
+export default class extends Vue {
+
+
+}
+</script>
+
+<style></style>
