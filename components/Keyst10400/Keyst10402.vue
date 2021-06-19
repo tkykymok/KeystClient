f1e9aa0071c3002f6516e24bde12bf39b36b9217
75ks <kenta.s705@gmail.com>
2021-06-19 12:03:43 +0900

    プロフィール画像モーダルを作成した

--- components/Keyst10400/Keyst10402.vue
+++ components/Keyst10400/Keyst10402.vue
@@ -30,8 +30,10 @@
       </tr>
       <tr class="p-2 flex justify-between items-center border-b-2">
         <th class="w-1/6 flex justify-center items-center">
-          <img src="/_nuxt/assets/img/user.png" alt="" class="w-12 h-12 mr-4 rounded-full border-none shadow-lg">
-          関根健太
+          <button @click="showImage=true">
+            <img src="/_nuxt/assets/img/user.png" alt="" class="w-12 h-12 rounded-full border-none shadow-lg">
+          </button>
+          <p class="ml-4">関根健太</p>
         </th>
         <th class="w-1/6">チームA</th>
         <th class="w-1/6">Java, AWS</th>
@@ -42,23 +44,27 @@
       </tr>
     </table>
     <Keyst10403 @closeModal="show=false" v-if="show" />
+    <Keyst10404 @closeModalImage="showImage=false" v-if="showImage" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
 import Keyst10403 from '~/components/Keyst10400/Keyst10403.vue';
+import Keyst10404 from '~/components/Keyst10400/Keyst10404.vue';
 
 @Component({
   name: 'Keyst10402',
   components: {
     Keyst10403,
+    Keyst10404,
   },
 })
 export default class extends Vue {
   data() {
     return {
       show: false,
+      showImage: false,
     }
   }
 }
