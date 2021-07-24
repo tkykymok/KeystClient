74c9bddd7586e51f8a7695e82a9386e7399d10c2
takayukiyamaoka <tkykymok@me.com>
2021-07-24 20:21:49 +0900

    排他制御カラムのフィールドを削除した

--- classes/skillSheetDetail.ts
+++ classes/skillSheetDetail.ts
@@ -17,5 +17,4 @@ export default class SkillSheetDetail {
   sowImplementation: boolean = false;
   sowTest: boolean = false;
   sowMaintenanceOperation: boolean = false;
-  versionExKey: number | null = null;
 }
