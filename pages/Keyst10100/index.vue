9f0edf2062f3b4482af9bcad9cd04d55ab42ac0c
takayukiyamaoka <tkykymok@me.com>
2021-08-01 13:55:12 +0900

    未修正箇所のボタンをコメントアウトした

--- pages/Keyst10100/index.vue
+++ pages/Keyst10100/index.vue
@@ -8,13 +8,17 @@
           <input v-if="inputAreaControl.userName.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.userName }}</span>
         </td>
-        <td class="p-2"><button @click="switchEditableFlag(inputAreaControl.userName)">aa</button></td>
+        <td class="p-2">
+          <button @click="switchEditableFlag(inputAreaControl.userName)">aa</button>
+        </td>
         <th class="p-2 bg-green-300 border-2 border-gray">郵便番号</th>
         <td colspan="2" class="p-2 border-2 border-gray">
           <span v-if="postalCode">332-0012</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="postalCodeBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="postalCodeBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <th class="p-2 bg-green-300 border-2 border-gray">氏名(カナ)</th>
@@ -22,13 +26,17 @@
           <span v-if="nameKana">エダモモコ</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="nameKanaBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="nameKanaBtn">aa</button>-->
+        </td>
         <th class="p-2 bg-green-300 border-2 border-gray">住所</th>
         <td colspan="2" class="p-2 border-2 border-gray">
           <span v-if="address">埼玉県川口市１１１ー１１１</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="addressBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="addressBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <th class="p-2 bg-green-300 border-2 border-gray">メールアドレス</th>
@@ -36,13 +44,17 @@
           <span v-if="mail">m.eda@c4c.co.jp</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="mailBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="mailBtn">aa</button>-->
+        </td>
         <th class="p-2 bg-green-300 border-2 border-gray">電話番号</th>
         <td colspan="2" class="p-2 border-2 border-gray">
           <span v-if="phoneNumber">09012345678</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="phoneNumberBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="phoneNumberBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <th class="p-2 bg-green-300 border-2 border-gray">ログインID</th>
@@ -50,14 +62,18 @@
           <span v-if="loginId">momokoeda</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="loginIdBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="loginIdBtn">aa</button>-->
+        </td>
         <th rowspan="6" class="p-2 bg-green-300 border-2 border-gray">銀行口座</th>
         <td class="p-2 border-2 border-gray">銀行名</td>
         <td class="p-2 border-2 border-gray">
           <span v-if="bankName">埼玉銀行</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="bankNameBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="bankNameBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <th class="p-2 bg-green-300 border-2 border-gray">ログインパスワード</th>
@@ -65,13 +81,17 @@
           <span v-if="loginPw">**************</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="loginPwBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="loginPwBtn">aa</button>-->
+        </td>
         <td class="p-2 border-2 border-gray">支店名</td>
         <td class="p-2 border-2 border-gray">
           <span v-if="branchName">川口支店</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="branchNameBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="branchNameBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <th rowspan="3" class="p-2 bg-green-300 border-2 border-gray">プロフィール画像</th>
@@ -79,13 +99,17 @@
           <img v-if="prfImgStrgDrctry" src="*" />
           <input type="file" v-else value="" />
         </td>
-        <td rowspan="3" class="p-2"><button @click="prfImgStrgDrctryBtn">aa</button></td>
+        <td rowspan="3" class="p-2">
+<!--          <button @click="prfImgStrgDrctryBtn">aa</button>-->
+        </td>
         <td class="p-2 border-2 border-gray">支店番号</td>
         <td class="p-2 border-2 border-gray">
           <span v-if="branchId">999</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="branchIdBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="branchIdBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <td class="p-2 border-2 border-gray">口座種別</td>
@@ -93,7 +117,9 @@
           <span v-if="accountType">普通預金</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="accountTypeBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="accountTypeBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <td class="p-2 border-2 border-gray">口座番号</td>
@@ -101,7 +127,9 @@
           <span v-if="accountNumber">12345678</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="accountNumberBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="accountNumberBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <th class="p-2 bg-green-300 border-2 border-gray">性別</th>
@@ -115,7 +143,9 @@
           <span v-if="accountName">エダモモコ</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td class="p-2"><button @click="accountNameBtn">aa</button></td>
+        <td class="p-2">
+<!--          <button @click="accountNameBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <th class="p-2 bg-green-300 border-2 border-gray">生年月日</th>
@@ -123,7 +153,9 @@
           <span v-if="birthday">1995/10/20</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td colspan="5" class="p-2"><button @click="birthdayBtn">aa</button></td>
+        <td colspan="5" class="p-2">
+<!--          <button @click="birthdayBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <th class="p-2 bg-green-300 border-2 border-gray">国籍</th>
@@ -131,7 +163,9 @@
           <span v-if="nationality">日本</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td colspan="5" class="p-2"><button @click="nationalityBtn">aa</button></td>
+        <td colspan="5" class="p-2">
+<!--          <button @click="nationalityBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <th class="p-2 bg-green-300 border-2 border-gray">配偶者</th>
@@ -147,7 +181,9 @@
           <span v-if="nearestStation">川口</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td colspan="5" class="p-2"><button @click="nearestStationBtn">aa</button></td>
+        <td colspan="5" class="p-2">
+<!--          <button @click="nearestStationBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <th class="p-2 bg-green-300 border-2 border-gray">最終学歴年月日</th>
@@ -155,7 +191,9 @@
           <span v-if="finalEducationDate">2019年3月</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td colspan="5" class="p-2"><button @click="finalEducationDateBtn">aa</button></td>
+        <td colspan="5" class="p-2">
+<!--          <button @click="finalEducationDateBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <th class="p-2 bg-green-300 border-2 border-gray">最終学歴</th>
@@ -163,7 +201,9 @@
           <span v-if="finalEducationContent">津田塾大学◯◯学部</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td colspan="5" class="p-2"><button @click="finalEducationContentBtn">aa</button></td>
+        <td colspan="5" class="p-2">
+<!--          <button @click="finalEducationContentBtn">aa</button>-->
+        </td>
       </tr>
       <tr class="">
         <th class="p-2 bg-green-300 border-2 border-gray">保有スキル</th>
@@ -171,7 +211,9 @@
           <span v-if="skills">Java</span>
           <input type="text" v-else class="bg-yellow-200" value="" />
         </td>
-        <td colspan="5" class="p-2"><button @click="skillsBtn">aa</button></td>
+        <td colspan="5" class="p-2">
+<!--          <button @click="skillsBtn">aa</button>-->
+        </td>
       </tr>
     </table>
     <input type="button" class="block p-2 w-40 m-5 mx-auto rounded bg-blue-200" value="登録" />
