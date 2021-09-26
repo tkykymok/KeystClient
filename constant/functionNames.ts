4ac2545edfd05d999e086f384a5f446ed041c395
momokoeda <m.eda@c4c.co.jp>
2021-09-26 21:25:10 +0900

    スタイル修正

--- constant/functionNames.ts
+++ constant/functionNames.ts
@@ -1,4 +1,5 @@
 export const FunctionNames = {
+  KEYST10100: { id: 'KEYST10100', name: 'プロフィール' },
   KEYST10200: { id: 'KEYST10200', name: 'スキルシート' },
   KEYST10300: { id: 'KEYST10300', name: '1on1予約' },
   KEYST10400: { id: 'KEYST10400', name: 'メンバー一覧' },
--- pages/Keyst10100/index.vue
+++ pages/Keyst10100/index.vue
@@ -3,224 +3,167 @@
     <h1 class="p-4 font-bold">プロフィール</h1>
     <table class="w-9/12 mx-auto text-left min-w-full text-xs">
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">氏名</th>
-        <td class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">氏名</th>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.userName)">
           <input v-if="inputAreaControl.userName.editableFlag" type="text" class="bg-yellow-200" value="" v-model="memberInfo.userName" />
           <span v-else>{{ memberInfo.userName }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.userName)">aa</button>
-        </td>
-        <th class="p-2 bg-green-300 border-2 border-gray">郵便番号</th>
-        <td colspan="2" class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">郵便番号</th>
+        <td colspan="2" class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.postalCode)">
           <input v-if="inputAreaControl.postalCode.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.postalCode }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.postalCode)">aa</button>
-        </td>
       </tr>
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">氏名(カナ)</th>
-        <td class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">氏名(カナ)</th>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.userNameKana)">
           <input v-if="inputAreaControl.userNameKana.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.userNameKana }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.userNameKana)">aa</button>
-        </td>
-        <th class="p-2 bg-green-300 border-2 border-gray">住所</th>
-        <td colspan="2" class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">住所</th>
+        <td colspan="2" class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.address)">
           <input v-if="inputAreaControl.address.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.address }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.address)">aa</button>
-        </td>
       </tr>
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">メールアドレス</th>
-        <td class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">メールアドレス</th>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.email)">
           <input v-if="inputAreaControl.email.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.email }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.email)">aa</button>
-        </td>
-        <th class="p-2 bg-green-300 border-2 border-gray">電話番号</th>
-        <td colspan="2" class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">電話番号</th>
+        <td colspan="2" class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.phoneNumber)">
           <input v-if="inputAreaControl.phoneNumber.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.phoneNumber }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.phoneNumber)">aa</button>
-        </td>
       </tr>
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">ログインID</th>
-        <td class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">ログインID</th>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.loginId)">
           <input v-if="inputAreaControl.loginId.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.loginId }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.loginId)">aa</button>
-        </td>
-        <th rowspan="6" class="p-2 bg-green-300 border-2 border-gray">銀行口座</th>
-        <td class="p-2 border-2 border-gray">銀行名</td>
-        <td class="p-2 border-2 border-gray">
+        <th rowspan="6" class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">銀行口座</th>
+        <td class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">銀行名</td>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.bankName)">
           <input v-if="inputAreaControl.bankName.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.bankName }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.bankName)">aa</button>
-        </td>
       </tr>
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">ログインパスワード</th>
-        <td class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">ログインパスワード</th>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.loginPw)">
           <input v-if="inputAreaControl.loginPw.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-if="inputAreaControl.loginPw.editableFlag">　確認用:</span>
           <input v-if="inputAreaControl.loginPw.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>●●●●●●●●●</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.loginPw)">aa</button>
-        </td>
-        <td class="p-2 border-2 border-gray">支店名</td>
-        <td class="p-2 border-2 border-gray">
+        <td class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">支店名</td>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.branchName)">
           <input v-if="inputAreaControl.branchName.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.branchName }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.branchName)">aa</button>
-        </td>
       </tr>
       <tr class="">
-        <th rowspan="3" class="p-2 bg-green-300 border-2 border-gray">プロフィール画像</th>
-        <td rowspan="3" class="p-2 border-2 border-gray">
+        <th rowspan="3" class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">プロフィール画像</th>
+        <td rowspan="3" class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.prfImgStrgDrctry)">
           <!-- <input v-if="inputAreaControl.prfImgStrgDrctry.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.prfImgStrgDrctry }}</span> -->
           <input v-if="inputAreaControl.prfImgStrgDrctry.editableFlag" type="file" value="" />
           <img :src="memberInfo.prfImgStrgDrctry" v-else>
         </td>
-        <td rowspan="3" class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.prfImgStrgDrctry)">aa</button>
-        </td>
-        <td class="p-2 border-2 border-gray">支店番号</td>
-        <td class="p-2 border-2 border-gray">
+        <td class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">支店番号</td>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.branchId)">
           <input v-if="inputAreaControl.branchId.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.branchId }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.branchId)">aa</button>
-        </td>
       </tr>
       <tr class="">
-        <td class="p-2 border-2 border-gray">口座種別</td>
-        <td class="p-2 border-2 border-gray">
+        <td class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">口座種別</td>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.accountType)">
           <input v-if="inputAreaControl.accountType.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.accountType }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.accountType)">aa</button>
-        </td>
       </tr>
       <tr class="">
-        <td class="p-2 border-2 border-gray">口座番号</td>
-        <td class="p-2 border-2 border-gray">
+        <td class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">口座番号</td>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.accountNumber)">
           <input v-if="inputAreaControl.accountNumber.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.accountNumber }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.accountNumber)">aa</button>
-        </td>
       </tr>
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">性別</th>
-        <td class="p-2 border-2 border-gray">
-          <input type="radio" id="men" value="1" :checked="memberInfo.gender === 1"><label for="men">男</label>
-          <input type="radio" id="women" value="2" :checked="memberInfo.gender === 2"><label for="women">女</label>
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">性別</th>
+        <td class="p-3 text-gray-800 border border-b">
+          <input type="radio" id="men" name="gender" value="1" :checked="memberInfo.gender === 1"><label for="men">男</label>
+          <input type="radio" id="women" name="gender" value="2" :checked="memberInfo.gender === 2"><label for="women">女</label>
         </td>
-        <td class="p-2"></td>
-        <td class="p-2 border-2 border-gray">名義人(カナ)</td>
-        <td class="p-2 border-2 border-gray">
+        <td class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">名義人(カナ)</td>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.accountName)">
           <input v-if="inputAreaControl.accountName.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.accountName }}</span>
         </td>
-        <td class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.accountName)">aa</button>
-        </td>
       </tr>
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">生年月日</th>
-        <td class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">生年月日</th>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.birthday)">
           <input v-if="inputAreaControl.birthday.editableFlag" type="text" class="bg-yellow-200" value="" />
-          <span v-else>{{ memberInfo.birthday }}</span>
-        </td>
-        <td colspan="5" class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.birthday)">aa</button>
+          <span v-else>{{ convertDateToYearMonthDay(memberInfo.birthday) }}</span>
         </td>
       </tr>
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">国籍</th>
-        <td class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">国籍</th>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.nationality)">
           <input v-if="inputAreaControl.nationality.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.nationality }}</span>
         </td>
-        <td colspan="5" class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.nationality)">aa</button>
-        </td>
       </tr>
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">配偶者</th>
-        <td class="p-2 border-2 border-gray">
-          <input type="radio" id="partnerOn" value="true" :checked="memberInfo.partnerFlg === true"><label for="partnerOn">あり</label>
-          <input type="radio" id="partnerOff" value="false" :checked="memberInfo.partnerFlg === false"><label for="partnerOff">なし</label>
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">配偶者</th>
+        <td class="p-3 text-gray-800 border border-b">
+          <input type="radio" id="partnerOn" name="partnerFlg" value="true" :checked="memberInfo.partnerFlg === true"><label for="partnerOn">あり</label>
+          <input type="radio" id="partnerOff" name="partnerFlg" value="false" :checked="memberInfo.partnerFlg === false"><label for="partnerOff">なし</label>
         </td>
         <td colspan="5" class="p-2"></td>
       </tr>
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">最寄り駅</th>
-        <td class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">最寄り駅</th>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.nearestStation)">
           <input v-if="inputAreaControl.nearestStation.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.nearestStation }}</span>
         </td>
-        <td colspan="5" class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.nearestStation)">aa</button>
-        </td>
       </tr>
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">最終学歴年月日</th>
-        <td class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">最終学歴年月日</th>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.finalEducationDate)">
           <input v-if="inputAreaControl.finalEducationDate.editableFlag" type="text" class="bg-yellow-200" value="" />
-          <span v-else>{{ memberInfo.finalEducationDate }}</span>
-        </td>
-        <td colspan="5" class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.finalEducationDate)">aa</button>
+          <span v-else>{{ convertDateToYearMonthDay(memberInfo.finalEducationDate) }}</span>
         </td>
       </tr>
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">最終学歴</th>
-        <td class="p-2 border-2 border-gray">
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">最終学歴</th>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.finalEducationContent)">
           <input v-if="inputAreaControl.finalEducationContent.editableFlag" type="text" class="bg-yellow-200" value="" />
           <span v-else>{{ memberInfo.finalEducationContent }}</span>
         </td>
-        <td colspan="5" class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.finalEducationContent)">aa</button>
-        </td>
       </tr>
       <tr class="">
-        <th class="p-2 bg-green-300 border-2 border-gray">保有スキル</th>
-        <td class="p-2 border-2 border-gray">
-          <input v-if="inputAreaControl.skillList.editableFlag" type="text" class="bg-yellow-200" value="" />
-          <span v-else v-for='skill in memberInfo.skillList' :key="skill.skillCode">{{ skill.skillName }} </span>
-        </td>
-        <td colspan="5" class="p-2">
-          <button @click="switchEditableFlag(inputAreaControl.skillList)">aa</button>
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 w-2/12">保有スキル</th>
+        <td class="p-3 text-gray-800 border border-b" @click="switchEditableFlag(inputAreaControl.skillList)">
+          <select  v-if="inputAreaControl.skillList.editableFlag">
+            <option>aa</option>
+            <option>aa</option>
+            <option>aa</option>
+            <option>aa</option>
+            <option>aa</option>
+            <option>aa</option>
+          </select>
+          <span v-else v-for='skill in memberInfo.skillList' :key="skill.skillCode" class="px-3 py-1 justify-around border-1 border-gray-100 bg-yellow-200 rounded-lg m-1">{{ skill.skillName }} </span>
         </td>
       </tr>
     </table>
-    <input type="button" class="block p-2 w-40 m-5 mx-auto rounded bg-blue-200" value="登録" />
+    <input type="button" class="block p-2 w-40 m-5 mx-auto bg-blue-400 text-white rounded-md hover:bg-blue-500 active:outline-none focus:outline-none" value="登録" />
   </div>
 </template>
 
@@ -228,6 +171,7 @@
 import { Component, Vue } from 'nuxt-property-decorator';
 import { Keyst10100Module } from '~/utils/store-accessor';
 import MemberInfo from '~/classes/memberInfo';
+import { convertDateToYearMonth, convertDateToYearMonthDay } from '~/utils/converter';
 
 @Component({
   name: 'Keyst10100',
@@ -255,7 +199,7 @@ export default class extends Vue {
     return Keyst10100Module.MemberInfo.prfImgStrgDrctry;
   }
 
-    /**
+  /**
    * 編集可否フラグを切り替える
    * @param obj
    */
@@ -287,6 +231,10 @@ export default class extends Vue {
     skillList:{editableFlag:false},
   }
 
+  /**
+   * yyyy年MM月dd日の形式に変換する
+   */
+  public convertDateToYearMonthDay = convertDateToYearMonthDay;
 }
 
 </script>
